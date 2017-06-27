63191e9ed2bbc9902110a8cbce49f1427427e679
machenchi0207 <415401620@qq.com>
2017-06-27 12:24:12 +0800

    initial

---
+++ src/SWIFServiceProvider.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Macc\Laravel\SWIF;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Cache;
+
+class SWIFServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'swif.php' => config_path('swif.php'),
+        ]);
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('macc.swif.filter', function ($app) {
+            $memcache = app()->make('memcached.connector');
+            $wordsAdapter = new \SIWF\Words\FileWordsAdapter(config('swif.blacklist.path'));
+            $builder = new \SIWF\Tree\Builder($wordsAdapter);
+            $storage = new \SIWF\Storage\MemcacheStorageAdapter($builder,$memcache);
+            $resStorage = new \SIWF\Filter\Result\MemcacheAdapter($memcache);
+//update words list
+            if(file_exists(config('swif.blacklist.path')))
+            {
+                $mtime = filemtime(config('swif.blacklist.path'));
+
+                if($mtime - $memcache->get('siwf_blacklist_create_time') >0)
+                {
+                    $memcache->set('siwf_blacklist_create_time',time());
+                    $storage->clear();
+                    $resStorage->clear();
+                }
+            }
+
+
+//$filter = new \SIWF\Filter\Filter($storage);
+            $filter = new \SIWF\Filter\CachedFilter($storage,$resStorage);
+
+            return $filter;
+        });
+    }
+}
