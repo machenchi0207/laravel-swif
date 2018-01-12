dd44eab25cb2aab27231b19660b634037b2f6ea1
machenchi <machenchi0207@gmail.com>
2018-01-12 19:40:22 +0800

    optimize

--- src/SWIFServiceProvider.php
+++ src/SWIFServiceProvider.php
@@ -27,15 +27,15 @@ class SWIFServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->singleton('macc.swif.filter', function ($app) {
-            $cache = app('cache');
-            $memcached = $cache->store('memcached')->getStore()->getMemcached();
-            $wordsAdapter = new \SIWF\Words\FileWordsAdapter(config('swif.blacklist.path'));
-            $builder = new \SIWF\Tree\Builder($wordsAdapter);
-            $storage = new \SIWF\Storage\MemcachedStorageAdapter($builder,$memcached);
-            $resStorage = new \SIWF\Filter\Result\MemcachedAdapter($memcached);
 //update words list
             if(file_exists(config('swif.blacklist.path')))
             {
+                $cache = app('cache');
+                $memcached = $cache->store('memcached')->getStore()->getMemcached();
+                $wordsAdapter = new \SIWF\Words\FileWordsAdapter(config('swif.blacklist.path'));
+                $builder = new \SIWF\Tree\Builder($wordsAdapter);
+                $storage = new \SIWF\Storage\MemcachedStorageAdapter($builder,$memcached);
+                $resStorage = new \SIWF\Filter\Result\MemcachedAdapter($memcached);
                 $mtime = filemtime(config('swif.blacklist.path'));
 
                 if($mtime - $memcached->get('siwf_blacklist_create_time') >0)
@@ -44,6 +44,8 @@ class SWIFServiceProvider extends ServiceProvider
                     $storage->clear();
                     $resStorage->clear();
                 }
+            }else{
+                throw new \Exception('Not found black list dic file!');
             }
 
 
